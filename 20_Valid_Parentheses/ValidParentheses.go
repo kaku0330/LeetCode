51e3d40cb3e66c91435e39241091956301e77a43
kaku0330 <kaku880330@gmail.com>
2024-05-16 13:40:34 +0800

    docs:精簡寫法

--- 20_Valid_Parentheses/ValidParentheses.go
+++ 20_Valid_Parentheses/ValidParentheses.go
@@ -1,43 +1,73 @@
 package validParentheses
 
-func test(s string) bool {
+func IsValid(s string) bool {
+	//第一次
+	// var first []rune
+	// if len(s)%2 != 0 {
+	// 	return false
+	// }
+	// for _, element := range s {
+	// 	if element == 0x28 || element == 0x5B || element == 0x7B {
+	// 		first = append(first, element)
+	// 	} else if element == 0x29 || element == 0x5D || element == 0x7D {
+	// 		if len(first) != 0 {
+	// 			if first[len(first)-1] == 0x28 {
+	// 				if element == 0x29 {
+	// 					first = first[0 : len(first)-1]
+	// 				} else {
+	// 					return false
+	// 				}
+	// 			} else if first[len(first)-1] == 0x5B {
+	// 				if element == 0x5D {
+	// 					first = first[0 : len(first)-1]
+	// 				} else {
+	// 					return false
+	// 				}
+	// 			} else if first[len(first)-1] == 0x7B {
+	// 				if element == 0x7D {
+	// 					first = first[0 : len(first)-1]
+	// 				} else {
+	// 					return false
+	// 				}
+	// 			}
+	// 		} else {
+	// 			return false
+	// 		}
+	// 	}
+	// }
+	// if len(first) == 0 {
+	// 	return true
+	// } else {
+	// 	return false
+	// }
 
+	//第二次
 	var first []rune
+	check := map[rune]rune{
+		0x29: 0x28,
+		0x5D: 0x5B,
+		0x7D: 0x7B,
+	}
+
 	if len(s)%2 != 0 {
 		return false
 	}
+
 	for _, element := range s {
-		if element == 0x28 || element == 0x5B || element == 0x7B {
+		switch element {
+		case 0x28, 0x5B, 0x7B:
 			first = append(first, element)
-		} else if element == 0x29 || element == 0x5D || element == 0x7D {
+		case 0x29, 0x5D, 0x7D:
 			if len(first) != 0 {
-				if first[len(first)-1] == 0x28 {
-					if element == 0x29 {
-						first = first[0 : len(first)-1]
-					} else {
-						return false
-					}
-				} else if first[len(first)-1] == 0x5B {
-					if element == 0x5D {
-						first = first[0 : len(first)-1]
-					} else {
-						return false
-					}
-				} else if first[len(first)-1] == 0x7B {
-					if element == 0x7D {
-						first = first[0 : len(first)-1]
-					} else {
-						return false
-					}
+				if first[len(first)-1] != check[element] {
+					return false
+				} else {
+					first = first[0 : len(first)-1]
 				}
 			} else {
 				return false
 			}
 		}
 	}
-	if len(first) == 0 {
-		return true
-	} else {
-		return false
-	}
+	return len(first) == 0
 }
--- main.go
+++ main.go
@@ -1,5 +1,12 @@
 package main
 
+import (
+	validParentheses "LeetCode/20_Valid_Parentheses"
+	"fmt"
+)
+
 func main() {
+	s := "))"
+	fmt.Println(validParentheses.IsValid(s))
 
 }
